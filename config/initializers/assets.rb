2c882d5f89dfd9c7cb11c257ebedcb9a9acbba98
gy0p4k <gyorgypakozdi@gmail.com>
2017-11-09 19:42:29 +0100

    feat(new_mesage): added style

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -14,4 +14,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Rails.application.config.assets.precompile += %w( admin.js admin.css )
 
 
-Rails.application.config.assets.precompile += %w( notes.scss )
+Rails.application.config.assets.precompile += %w( new.css )
