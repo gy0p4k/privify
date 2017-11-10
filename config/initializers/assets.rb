edb64422228144afda3ad69de285619d730e33b5
gy0p4k <gyorgypakozdi@gmail.com>
2017-11-10 15:15:48 +0100

    feat(style) add more and more style

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -15,3 +15,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
 
 Rails.application.config.assets.precompile += %w( new.css )
+Rails.application.config.assets.precompile += %w( notes.css )
